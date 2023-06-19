fc37151e273192e27f5593c3346d11e44e526c9d
Hussien-Haidar <72030603@students.liu.edu.lb>
2023-06-19 06:02:44 +0300

    Commit

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom'
 import { logout } from '../redux/slices/userSlice'
-import { useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Navbar = () => {
+    const {user} = useSelector(state => state.user)
     const dispatch = useDispatch()
 
     const handleClick = () => {
@@ -16,13 +17,18 @@ const Navbar = () => {
                     <h1>Workout Buddy</h1>
                 </Link>
                 <nav>
-                    <div>
-                        <button onClick={handleClick}>Log out</button>
-                    </div>
-                    <div>
-                        <Link to="/login">Login</Link>
-                        <Link to="/signup">Sign up</Link>
-                    </div>
+                    {user && (
+                        <div>
+                            <span>{user.email}</span>
+                            <button onClick={handleClick}>Log out</button>
+                        </div>
+                    )}
+                    {!user && (
+                        <div>
+                            <Link to="/login">Login</Link>
+                            <Link to="/signup">Sign up</Link>
+                        </div>
+                    )}
                 </nav>
             </div>
         </header>
